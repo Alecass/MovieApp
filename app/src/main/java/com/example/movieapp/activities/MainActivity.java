2481c9320fe5274c19a653b4da03e2295ab093d3
Alessio <49764709+Alecass@users.noreply.github.com>
2020-05-02 11:51:00 +0200

    Add Grid View

--- app/src/main/java/com/example/movieapp/activities/MainActivity.java
+++ app/src/main/java/com/example/movieapp/activities/MainActivity.java
@@ -3,7 +3,6 @@ package com.example.movieapp.activities;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 import androidx.loader.app.LoaderManager;
 import androidx.loader.content.CursorLoader;
 import androidx.loader.content.Loader;
@@ -13,10 +12,7 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
+import android.widget.GridView;
 import android.widget.Toast;
 
 import com.example.movieapp.R;
@@ -24,9 +20,6 @@ import com.example.movieapp.adapters.FilmAdapter;
 import com.example.movieapp.database.FilmDB;
 import com.example.movieapp.database.FilmProvider;
 import com.example.movieapp.database.FilmTableHelper;
-
-import java.lang.reflect.Array;
-import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
 
@@ -40,16 +33,15 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
     SQLiteDatabase database;
 
     Cursor filmItems;
-    ListView filmList;
+    GridView filmList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        filmList = findViewById(R.id.list_view);
+        filmList = findViewById(R.id.filmGrid);
         filmAdapter = new FilmAdapter(this,null);
         filmList.setAdapter(filmAdapter);
         getSupportLoaderManager().initLoader(MY_ID, null, this);
-
         insertFilm();
     }
     private void insertFilm(){
